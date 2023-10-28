440ef6329af59c5466670ab207cbc8fc3f3bae8d
Radyusha <Al.shel@bk.ru>
2023-10-28 15:27:42 +0300

    module 2

--- module03/src/main/java/ru/sberbank/edu/App.java
+++ module03/src/main/java/ru/sberbank/edu/App.java
@@ -10,12 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        CustomArrayImpl<Integer> list = new CustomArrayImpl<>();
-        list.add(1);
-        list.add(2);
-        list.add(2);
-        list.add(3);
-
-        System.out.println(list );
+        System.out.println("Привет" );
     }
 }
