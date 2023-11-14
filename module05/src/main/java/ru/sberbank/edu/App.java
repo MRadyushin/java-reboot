4588091d17b872e7193ed2765cf6b676e97be4d2
Radyusha <Al.shel@bk.ru>
2023-11-14 23:20:56 +0300

    module 5

--- module05/src/main/java/ru/sberbank/edu/App.java
+++ module05/src/main/java/ru/sberbank/edu/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        GeoPosition city3 = new GeoPosition("55", "55(45'07'')");
+
+        System.out.println(city3.getLongitude());
+        System.out.println(city3.getLatitude());
     }
 }
